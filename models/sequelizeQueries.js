87b42ba4f8da46cb0cdf26a48cf2299d6674e3ec
FSOCNS-Intern-Torres <FSOCNSIntern.TORRES@ph.ey.com>
2022-09-28 08:18:51 +0800

    can connect to database already

---
+++ models/sequelizeQueries.js
@@ -0,0 +1,21 @@
+import server from "../server.js";
+
+console.log(server)
+// const selectQuery = async ()=>{
+//    let result1 = await server.models.AdditionalInfo.findAll({raw:true})
+//    let result2 = await server.models.AdditionalInfo.findAll({
+//       where: {
+//          _id: 1
+//       },
+//       raw: true
+//    })
+
+//    console.log(result1)
+// }
+
+// selectQuery();
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,26 +1,119 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(
-        "sequelize_tuts_db", //database name
-        "root",             //user 
-        "fsocns!ntern22",   //password
-        {
-            host: "127.0.0.1",
-            dialect: "mysql"
-        }
-)
+//const Sequelize = require('sequelize');
+//import {Sequelize, Op, DataTypes, Model} from "sequelize";
+import Sequelize from "sequelize";
+import { UUIDV4 } from "sequelize";
+import {Model, DataTypes, Op} from 'sequelize';
 
 
-// sequelize.authenticate().then(() => {
-//    console.log('Connection has been established successfully.');
-// }).catch((error) => {
-//    console.error('Unable to connect to the database: ', error);
-// });
+const exportSequelize = () =>{
+   const sequelize = new Sequelize(
+      "sequelize_tuts_db", //database name
+      "root",             //user 
+      "fsocns!ntern22",   //password: 
+      {
+          host: "127.0.0.1", //
+          port: 3306,
+          dialect: "mysql"
+      }
+   )
 
-const sequelizeTest = async () => {
-    try { await sequelize.authenticate();
-    } catch (err) {
-        console.error("Unable to connect to the database: ", err);
-    }
+
+   const sequelizeTest = async () => {
+   try { 
+         await sequelize.authenticate();
+   } catch (err) {
+         console.error("Unable to connect to the database: ", err);
+   }
+   }
+   sequelizeTest();
+
+
+
+   class User extends Model {}
+   User.init(
+   {
+      _id:{
+         type: Sequelize.UUID,
+         primaryKey: true,
+         defaultValue: UUIDV4
+      },
+      username: {
+         type: Sequelize.STRING
+      },
+      password: {
+         type: Sequelize.STRING
+      },
+      age: {
+         type: Sequelize.INTEGER
+      }, 
+   },
+
+   { sequelize, modelName: 'User', tableName: 'userstable', timestamps: false}
+
+   )
+
+   class AdditionalInfo extends Model {}
+   AdditionalInfo.init(
+   {
+      _id:{
+         type: Sequelize.INTEGER,
+         primaryKey: true,
+         autoIncrement: true
+      },
+      birthDate: {
+         type: Sequelize.DATE
+      },
+      motherName: {
+         type: Sequelize.STRING
+      },
+      fatherName: {
+         type: Sequelize.STRING
+      }, 
+      userID:{
+         type: Sequelize.INTEGER
+      }
+   },
+
+   { sequelize, modelName: 'AdditionalInfo', tableName: 'existingtable', timestamps: false}
+
+   )
+
+   const syncThis = async ()=>{
+   try {
+      await sequelize.sync();
+   } catch (err){
+      console.log("error: ", err)
+   }
+   }
+
+   syncThis();
+
+   //return this.sequelize;
+   return sequelize;
 }
-sequelizeTest();
+
+
+export default exportSequelize();
+// const selectQuery = async ()=>{
+//    let result1 = await sequelize.models.AdditionalInfo.findAll({raw:true})
+//    let result2 = await sequelize.models.AdditionalInfo.findAll({
+//       where: {
+//          _id: 1
+//       },
+//       raw: true
+//    })
+
+//    console.log(result1)
+// }
+
+// selectQuery();
+
+
+
+
+
+
+
+
+
+
